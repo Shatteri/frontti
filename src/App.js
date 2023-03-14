0d4225d45bd81d6a551f8d7d150f87b410938309
Shatteri <elias.isopahkala@gmail.com>
2023-03-14 13:32:03 +0200

    react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 import './App.css';
 import TodoList from './TodoList';
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
+    <Link to="/">Home</Link>{' '}
+    <Link to="/about">About</Link>{' '}
+    <Link to="/contact">Contact</Link>{' '}
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
+      <div className="App">
       <TodoList />
+    </div>
+    </BrowserRouter>
     </div>
   );
 }
